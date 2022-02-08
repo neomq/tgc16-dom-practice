2c01e6f5ff2a4c01046858eed589aaa698822be9
neomq <neominqi@hotmail.com>
2022-02-08 17:17:49 +0000

    updated qn 6

--- q6_counter/script.js
+++ q6_counter/script.js
@@ -5,3 +5,13 @@ document.querySelector('#increment').addEventListener('click', function(){
     count += 1;
 });
 
+document.querySelector('#decrement').addEventListener('click', function(){
+    document.querySelector('#box').innerText = count;
+    count -= 1;
+});
+
+document.querySelector('#reset').addEventListener('click', function(){
+    count = 0;
+    document.querySelector('#box').innerText = count;
+});
+
